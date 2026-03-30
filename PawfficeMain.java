12a7ecd3e7d8a96daa7db3dc256f9ef269b61cde
basicallyblue <basicallyjunko@gmail.com>
2026-03-30 10:39:15 -0400

    trying to fix it not opening

--- PawfficeMain.java
+++ PawfficeMain.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 /**
  * Creates the actual panel for the user to interact with, still needs all the
@@ -12,6 +13,13 @@ import java.awt.Dimension;
  */
 
 public class PawfficeMain implements Runnable{
+
+    //user
+    private User user;
+
+    //testing stuff
+    private int x = 250;
+    private int y = 250;
 
     //background color
     private final Color BACKGROUND = new Color(0, 0, 0);
@@ -27,10 +35,24 @@ public class PawfficeMain implements Runnable{
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Pawffice");
         frame.setPreferredSize(new Dimension(500, 500));
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel = new JPanel();
-        panel.setBackground(BACKGROUND);
+        user = new User(250, 250);
+        
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                g.setColor(BACKGROUND);
+                g.fillRect(0, 0, getWidth(), getHeight());
+
+                user.draw(g);
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(500, 500));
         frame.add(panel);
     }
 
