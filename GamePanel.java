897e939df12e80626b733da6c7a4df9cacc54552
basicallyblue <basicallyjunko@gmail.com>
2026-03-30 10:06:27 -0400

    creating the start of the game panel for the start of our basic implementation

---
+++ GamePanel.java
@@ -0,0 +1,11 @@
+/**
+ * Creates the actual panel for the user to interact with, still needs all the
+ * implementation of the interactable stuff.
+ * 
+ * @author Team 10, the Bug Busters
+ * @version Spring, 2026
+ */
+
+public class GamePanel {
+    
+}
