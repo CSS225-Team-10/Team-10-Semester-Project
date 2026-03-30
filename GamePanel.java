d6edc02b019497f52c3c0cdf609d4423fd382f0d
basicallyblue <basicallyjunko@gmail.com>
2026-03-30 10:23:59 -0400

    starting the gamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,3 +1,6 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  * Creates the actual panel for the user to interact with, still needs all the
  * implementation of the interactable stuff.
@@ -6,6 +9,17 @@
  * @version Spring, 2026
  */
 
-public class GamePanel {
-    
+public class GamePanel implements Runnable{
+
+    /**
+     * The run method to set up the graphical user interface
+     */
+    @Override
+    public Run() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
