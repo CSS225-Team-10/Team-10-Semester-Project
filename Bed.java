33386f23c329f040d331fe96b32946c6abe57fe7
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-13 11:19:38 -0400

    added more tips to the tiplist...

--- Bed.java
+++ Bed.java
@@ -37,7 +37,7 @@ public class Bed extends RoomObject implements Runnable{
     public void run(){
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Pawffice Tips");
-        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setPreferredSize(new Dimension(300, 325));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
@@ -80,6 +80,15 @@ public class Bed extends RoomObject implements Runnable{
         tipList.add("If you are struggling to increase your positivity, one thing you can do is practice gratitude! What are some things you are grateful for?");
         tipList.add("Take a few deep breaths! You are allowed to take breaks. Everything will be okay.");
         tipList.add("Take some time to practice Self Care! You can try journalling, taking a walk, breathing, and so many other things! Learn what works best for you.");
+        tipList.add("Shoot for the moon, land amongst the stars.");
+        tipList.add("Prioritize consistent sleep. This will also help your immune system function, enhance brain performance, memory, but most importantly emotional regulation. If you are feeling overwhelmed with tasks, one of the best things you can do is sleep so that you are better prepared to have the brainpower to complete those tasks! Stay happy and healthy and get some rest!");
+        tipList.add("Spend some time in nature. You could take a walk, smell some flowers, sit by a tree, go on a picnic, build a snowman, ect. Whatever the weather permits. Nature can help you feel calmer.");
+        tipList.add("Physical health is very linked to mental health! Find a way to get active! Go on a short walk, play with your dog or cat, try yoga, or anything else you can think of. Just do whatever feels right for you!");
+        tipList.add("Take some time to de-stress and unwind. Find a hobby you enjoy and spend some time on that. You could go on a walk, color in a coloring page, hang out with friends, play a game, ect. This can help lower stress, cortisol levels, and decrease any anxiety or depression.");
+        tipList.add("Try a journalling exercise! You can even go do this by clicking on the bookshelf and taking notes there. Spend as much or as little time you need, and try writing down whatever has been on your mind. Take note of any differences in how you feel when you finish. Remember to be kind to yourself.");
+        tipList.add("If you have the energy for it, try to clean up your living area. It can be difficult coming back to a messy space when dealing with a lot of stress in your daily life. Clearer spaces can help your mind feel clearer. You can even help others while helping yourself! Clean out your old clothes, toys, books, etc. and compile them to donate!");
+        tipList.add("Try a breathing exercise to help you feel calm. One example is called box breathing. This is when you inhale for four seconds, hold for four seconds, exhale for four seconds, and hold one last time for four seconds. This process can be repeated as many times as needed. You can also switch out the numbers for whatever feels best for you.");
+        
 
         //ADD WAY MORE TIPS LATER :)
 
