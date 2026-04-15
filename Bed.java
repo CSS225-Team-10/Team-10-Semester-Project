d4ab8ee6819c594c5b5b9264c48058349a30843e
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-15 08:59:58 -0400

    added more to the tip list

--- Bed.java
+++ Bed.java
@@ -77,6 +77,10 @@ public class Bed extends RoomObject implements Runnable{
     private ArrayList<String> populateTips(){
         ArrayList<String> tipList = new ArrayList<>();
 
+        //https://www.carecredit.com/well-u/health-wellness/tips-improve-mental-health/
+        //https://mhanational.org/resources/31-tips-to-boost-your-mental-health/
+        //
+
         tipList.add("If you are struggling to increase your positivity, one thing you can do is practice gratitude! What are some things you are grateful for?");
         tipList.add("Take a few deep breaths! You are allowed to take breaks. Everything will be okay.");
         tipList.add("Take some time to practice Self Care! You can try journalling, taking a walk, breathing, and so many other things! Learn what works best for you.");
@@ -88,7 +92,11 @@ public class Bed extends RoomObject implements Runnable{
         tipList.add("Try a journalling exercise! You can even go do this by clicking on the bookshelf and taking notes there. Spend as much or as little time you need, and try writing down whatever has been on your mind. Take note of any differences in how you feel when you finish. Remember to be kind to yourself.");
         tipList.add("If you have the energy for it, try to clean up your living area. It can be difficult coming back to a messy space when dealing with a lot of stress in your daily life. Clearer spaces can help your mind feel clearer. You can even help others while helping yourself! Clean out your old clothes, toys, books, etc. and compile them to donate!");
         tipList.add("Try a breathing exercise to help you feel calm. One example is called box breathing. This is when you inhale for four seconds, hold for four seconds, exhale for four seconds, and hold one last time for four seconds. This process can be repeated as many times as needed. You can also switch out the numbers for whatever feels best for you.");
-        
+        tipList.add("If you are trying to feel better quickly, the best thing you can do is try getting your body moving as much or as often as possible. Excersize helps reduce symptoms of anxiety and depression. You don't have to run a marathon! You can walk your dog, go on a jog, or attend a fitness class. You can take some time to figure out what is best for you.");
+        tipList.add("If you're having trouble studying, try out clicking on the clock! This will bring you to a Pomodoro timer. If you're not sure what that is, it is a productivity tool that implements the Pomodoro Technique, which breaks work up into intervals of focus time called \"pomodoros\" seperated by breaks. The idea is to select a single task to focus on, and then set a timer, typically for about 25 minutes, and then work on the task you selected exclusively until the timer rings. Then, you can take a short break to stretch, grab water, practice some self care, ect. and repeat! Happy studying :)");
+        tipList.add("Healthy food is good for not only your body, but also your mind. There are associations between how you eat and how you feel. Healthy diets that have many fruits and veggies incorporated with lean protiens are best for mental health.");
+        tipList.add("Stay hydrated! Hydration helps make sure that your brain is working optimally. Dehydration can lead to many different health problems, including but not limited to reduced brain cogniiton, dizziness, fatigue, ect. Keep your favorite glass or water bottle handy while working! Take a walk to go refill it on the breaks from your pomodoro timer. Stay well, happy studying!");
+
 
         //ADD WAY MORE TIPS LATER :)
 
