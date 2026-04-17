73b67f2df6e96e9bcb8c87a65a6c518180bbca7d
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-17 00:50:32 -0400

    no implementation yet... decided to work on main instead

---
+++ Desk.java
@@ -0,0 +1,15 @@
+
+/**
+ * Note taking spot for Pawwfice
+ */
+public class Desk extends RoomObject implements Runnable{
+    
+    private static String savedNotes = "";
+
+    public Desk(){
+
+    }
+    //I was gonna write this code but i think instead I wanna get
+    //the clicking on pawwfice main working because this
+    //is gonna be really involved possibly with web dev stuff...
+}
