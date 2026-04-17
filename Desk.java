c82d1ad3f182a8179b83a788b093650a2eb95b49
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-17 01:02:05 -0400

    added methods with no real implementation in order to make code compile for Pawffice main to run

--- Desk.java
+++ Desk.java
@@ -9,6 +9,14 @@ public class Desk extends RoomObject implements Runnable{
     public Desk(){
 
     }
+
+    public boolean isClicked(int mouseX, int mouseY, int cameraX){
+        return false;
+    }
+
+    public void run(){
+        
+    }
     //I was gonna write this code but i think instead I wanna get
     //the clicking on pawwfice main working because this
     //is gonna be really involved possibly with web dev stuff...
