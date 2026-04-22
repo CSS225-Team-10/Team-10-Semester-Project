62a1bb799c00c4e0236e5f311e0d66f9532934a0
basicallyblue <basicallyjunko@gmail.com>
2026-04-22 07:55:00 -0400

    updated the bone image, no code changed

--- Bone.java
+++ Bone.java
@@ -1,16 +1,18 @@
+import java.awt.Graphics;
 import java.awt.Image;
-
 import javax.swing.ImageIcon;
 
+
 /**
- * Interactable bone, can be picked up, Baloo will follow and the bone will drop after being unclicked.
+ * Interactable bone, can be picked up, Baloo will follow and the bone will drop
+ * after being unclicked.
  * 
  * 
  * @author Team 10, the Bug Busters
  * @version Spring, 2026
  */
 
-public class Bone extends RoomObject{
+public class Bone extends RoomObject implements Runnable {
     /**
      * Instance variables
      */
@@ -19,7 +21,7 @@ public class Bone extends RoomObject{
     private int width = 40;
     private int height = 40;
 
-    private Image bookshelfImage;
+    private Image boneImage;
 
     /**
      * Constructor for the Bone class.
@@ -27,20 +29,20 @@ public class Bone extends RoomObject{
      * @param x x point
      * @param y y point
      */
-    public Bone(int x, int y){
+    public Bone(int x, int y) {
         super(x, y);
 
         isClicked = false;
-        ImageIcon icon = new ImageIcon("Images/bookshelf.png");
-        bookshelfImage = icon.getImage();
+        ImageIcon icon = new ImageIcon("Images/bone.png");
+        boneImage = icon.getImage();
     }
 
     /**
      * Checks if object is clicked at the point where the mouse is clicking.
      * Must be completed in all classes extending RoomObject.
      * 
-     * @param mouseX x point of mouse
-     * @param mouseY y point of mouse
+     * @param mouseX  x point of mouse
+     * @param mouseY  y point of mouse
      * @param cameraX x point of camera
      * @return
      */
@@ -52,9 +54,28 @@ public class Bone extends RoomObject{
     }
 
     /**
-     * This will launch the frames we want when we click on the different room objects
+     * Draws the bone on the screen.
+     * 
+     * @param g Graphics component
+     * @param cameraX the camera to draw in comparison to
+     */
+    public void draw(Graphics g, int cameraX) {
+        g.drawImage(boneImage, x - cameraX, y, width, height, null);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void run(){
+
+    }
+
+    /**
+     * This will launch the frames we want when we click on the different room
+     * objects
      */
     public void launch() {
-
+        javax.swing.SwingUtilities.invokeLater(new Bone(0, 0));
     }
 }
