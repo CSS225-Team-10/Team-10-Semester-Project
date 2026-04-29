cefb23f603a0c057bc9d63d619f65be6b7da5d54
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-28 20:51:05 -0400

    I need to test using codespaces... this code should hypothetically work?

--- express.js
+++ express.js
@@ -3,37 +3,35 @@
 const express = require('express');
 const app = express();
  
-
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
- 
 // Parse JSON request bodies (needed for POST)
 app.use(express.json());
+
+let savedNotes = "";
 
 // endpoints go below this line
 // General plan is for when they launch the notes frame, that
 // will be a GET request
-app.get('/savedNotes', (req, res)=>{
-  res.type('json').send({
-    messages
-  });
+app.get('/savedNotes', (req, res) => {
+  res.json({ notes: savedNotes });
 });
 
 
 // and when they click the save button,
 // that will be a POST request
 app.post('/savedNotes', (req, res) => {
-  if(!text || !author){
-    res.status(400).json({error: "IDK SOME ERROR MESSAGE"});
-  }else{
-    messages.push(
-        //idrk
-      {notes: savedNotes}
-    )
+  const { notes } = req.body;
+
+  if (!notes) {
+    return res.status(400).json({ error: "Notes are required" });
   }
+
+  savedNotes = notes;
+  res.json({ success: true });
+})
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
-
-
 
 
 
