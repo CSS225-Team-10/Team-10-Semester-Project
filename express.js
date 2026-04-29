90067b3b279f8ef099b78a46169a957ff928e099
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-28 20:52:42 -0400

    wrong port num

--- express.js
+++ express.js
@@ -30,7 +30,7 @@ app.post('/savedNotes', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:8080");
 });
 
 
